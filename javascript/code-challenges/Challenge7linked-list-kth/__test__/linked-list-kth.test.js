d24ea192fe4d5e88a6098d7b19c73c7afe7afc35
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-07-03 14:04:07 +0000

    solving challenge 7

--- javascript/code-challenges/Challenge7linked-list-kth/__test__/linked-list-kth.test.js
+++ javascript/code-challenges/Challenge7linked-list-kth/__test__/linked-list-kth.test.js
@@ -2,34 +2,37 @@ const LinkedList = require('../linked-list-kth');
 
 describe('Linked lists kth from end test function', () => {
     it('Where k is greater than the length of the linked list', () => {
-        let ll = new LinkedList;
+        let ll = new LinkedList();
         ll.insert(1);
         ll.insert(2);
         ll.insert(3);
         ll.insert(4);
         ll.insert(5);
-        expect(ll.kthFromEnd(15)).toEqual(`The input exceeds the length of the linkedList length is: ${ll.size}`); // k exceeds length case
-
+        expect(ll.kthFromEnd(6)).toEqual('k is greater than the length of the linked list');
     });
-    it('Where k and the length of the list are the same & Where k and the length of the list are the same & “Happy Path” where k is not at the end, but somewhere in the middle of the linked list', () => {
-        let ll = new LinkedList;
+
+    it('Where k is not a positive integer', () => {
+        let ll = new LinkedList();
+        ll.insert(1);
+        ll.insert(2);
+        ll.insert(3);
+        expect(ll.kthFromEnd(-2)).toEqual('k should be a positive integer');
+        expect(ll.kthFromEnd(0)).toEqual('k should be a positive integer');
+    });
+
+    it('Where the linked list is of size 1', () => {
+        let ll = new LinkedList();
+        ll.insert(1);
+        expect(ll.kthFromEnd(1)).toEqual(1);
+    });
+
+    it('Where k is in the middle of the linked list', () => {
+        let ll = new LinkedList();
         ll.insert(1);
         ll.insert(2);
         ll.insert(3);
         ll.insert(4);
         ll.insert(5);
-
-        expect(ll.kthFromEnd(4)).toEqual(5);
-        expect(ll.kthFromEnd(-4)).toEqual('Enter a positive number');
-        expect(ll.kthFromEnd(3)).toEqual(4);
-
+        expect(ll.kthFromEnd(3)).toEqual(3);
     });
-    it('Where the linked list is of a size 1', () => {
-        let ll = new LinkedList;
-        ll.insert(1);
-        expect(ll.kthFromEnd(0)).toEqual(1); // k equal to length.
-
-
-    });
-
 });
