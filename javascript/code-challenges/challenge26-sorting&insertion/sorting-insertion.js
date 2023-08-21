f02ba48cc54bc1e25d21015714f9fc2a81532e2d
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-08-21 17:10:13 +0000

    solving challenge 26

---
+++ javascript/code-challenges/challenge26-sorting&insertion/sorting-insertion.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function insert(SortedArr, intValue) {
+	let i = 0;
+	while (intValue > SortedArr[i]) {
+		i++;
+	}
+
+	while (i < SortedArr.length) {
+		let temp = SortedArr[i];
+		SortedArr[i] = intValue;
+		intValue = temp;
+		i++;
+	}
+	SortedArr.push(intValue);
+}
+
+function insertSorted(arr) {
+	let sorted = [];
+	sorted[0] = arr[0];
+
+	for (let i = 1; i < arr.length; i++) {
+		insert(sorted, arr[i]);
+	}
+
+	return sorted;
+}
+
+console.log(insertSorted([8, 4, 23, 42, 16, 15]));
